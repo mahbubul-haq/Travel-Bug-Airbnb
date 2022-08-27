81b6b18c25c32851055646e156cd2b8007282088
Mahbubul Haque <mdmahbubulhaque99@gmail.com>
2022-08-28 01:10:15 +0600

    Payment in host management and some fixes

--- travel-bug-backend/models/Booking.js
+++ travel-bug-backend/models/Booking.js
@@ -52,7 +52,10 @@ const BookingSchema = new Schema({
     min: 1,
     required: true,
   },
- 
+ paymentID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "PaymentInfo",
+ }
 
 });
 const Booking = mongoose.model("Booking", BookingSchema);
