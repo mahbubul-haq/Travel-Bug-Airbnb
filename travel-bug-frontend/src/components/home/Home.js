152551b380ca59e9606bf01d603024bc18946d3a
Maisha Rahman Mim <52733601+maisharahman876@users.noreply.github.com>
2022-08-28 08:29:55 +0600

    homepage refactor and payment redundancy resolved

--- travel-bug-frontend/src/components/home/Home.js
+++ travel-bug-frontend/src/components/home/Home.js
@@ -1,20 +1,12 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 import guideline from "../../images/guideline.jpg";
 import travel from "../../images/travel.jpg";
 import './home.css';
-import SearchBar from "../search/SearchBar";
 const Home = () => {
   let navigate = useNavigate();
-  const handleGuideline = (e) => {
-    e.preventDefault();
-    navigate("/transportguidelines");
-  }
-  const handleExperiences = (e) => {
-    e.preventDefault();
-    navigate("/experiences");
-  }
+  
   return (
     <div className="container">
       <center>
@@ -24,8 +16,9 @@ const Home = () => {
             <Card.Body><center><h1>Not Sure Where to Go?</h1></center></Card.Body>
 
           </Card.ImgOverlay>
-          <Card.Footer><center><Button variant="outline-dark" className="btn-lg" onClick={handleExperiences}>I'm Flexible</Button></center></Card.Footer>
-        </Card>
+          </Card>
+          <center><Link variant="outline-dark" className="btn btn-lg" to='/experiences'>I'm Flexible</Link></center>
+        
 
         <br />
         <br />
@@ -36,8 +29,9 @@ const Home = () => {
             <Card.Footer><center><h1>Need Guideline to Travel?</h1></center></Card.Footer>
 
           </Card.ImgOverlay>
-          <Card.Footer><center><Button variant="outline-dark" className="btn-lg" onClick={handleGuideline} >Go to Transport Guideline</Button></center></Card.Footer>
-        </Card>
+          </Card>
+          <center><Link variant="outline-dark" className="btn btn-lg" to='/transportguidelines' >Go to Transport Guideline</Link></center>
+        
       </center>
       <br />
       <br />
