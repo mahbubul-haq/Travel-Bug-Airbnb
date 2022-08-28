352da7f84b63f6e14c6ddaf0b1a9a00df08e85f4
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2022-08-28 06:48:21 +0600

    frontend location wise search and some fixes

--- travel-bug-frontend/src/components/experiences/SingleExperience.js
+++ travel-bug-frontend/src/components/experiences/SingleExperience.js
@@ -264,7 +264,7 @@ const SingleExperience = () => {
                       <h5 className="card-title">
                         Hosted By {host.firstName + " " + host.lastName}{" "}
                       </h5>
-                      <p className="card-text">Host Description:</p>
+                      <p className="card-text">Host Description: {host.userDetails}</p>
 
                       <p className="card-text">
                         <small className="text-muted">
@@ -354,7 +354,7 @@ const SingleExperience = () => {
                           <path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5V4z" />
                           <path d="M4.268 1A2 2 0 0 1 6 0h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L13 13.768V2a1 1 0 0 0-1-1H4.268z" />
                         </svg>{" "}
-                        Cancel before check-in on Jul 17 for a partial refund.
+                        Cancel before {experience.maxRefundDays} days for a partial refund.
                       </li>
                     </ul>
                   </Container>
